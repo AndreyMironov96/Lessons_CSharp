3dc6e343e82efcd691ff4e410bbbb96d87b237e0
Andrey Mironov <andrushamironov@icloud.com>
2019-05-11 00:03:55 +0400

    Start lessons

---
+++ Template_Lessons/Template_Lessons/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Template_Lessons
+{
+    class Product<T>
+    {
+        public string Name { get; }
+        public double Volume { get; }
+        public double Kalories { get; }
+        public Product(string name, double volume, double kalories)
+        {
+
+        }
+    }
+}
