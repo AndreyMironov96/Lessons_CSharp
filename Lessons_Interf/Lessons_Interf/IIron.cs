f7189c55e5f190a01f9bd23a190ed0cc6552f6f3
Andrey Mironov <andrushamironov@icloud.com>
2019-05-11 12:00:31 +0400

    Первый интерфейс

---
+++ Lessons_Interf/Lessons_Interf/IIron.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lessons_Interf
+{
+   internal interface IIron
+    {
+        double Degree (int mode);
+    }
+}
---
+++ Lessons_Interf/Lessons_Interf/Iron.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lessons_Interf
+{
+    class Iron : IIron
+    {
+        public double Degree(int mode)
+        {
+            return mode * 50;
+        }
+    }
+}
